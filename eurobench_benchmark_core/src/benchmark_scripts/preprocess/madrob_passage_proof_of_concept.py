1d9279a940f65902a17bdf9aa0807f845ef6c30a
Enrico Piazza <enry2077@gmail.com>
2020-01-13 13:26:02 +0100

    improved code, added test of moving average to remove noise from signals

--- eurobench_benchmark_core/src/benchmark_scripts/preprocess/madrob_passage_proof_of_concept.py
+++ eurobench_benchmark_core/src/benchmark_scripts/preprocess/madrob_passage_proof_of_concept.py
@@ -7,6 +7,9 @@ import message_filters
 from madrob_msgs.msg import Door, Passage
 from std_msgs.msg import Float64
 
+from collections import deque
+import numpy as np
+
 class MadrobPreprocess:
     def __init__(self):
         rospy.init_node("passage_test_node")
@@ -29,31 +32,13 @@ class MadrobPreprocess:
         self.door_proximity_timesync = message_filters.ApproximateTimeSynchronizer([self.cw_right_sub, self.cw_left_sub, self.ccw_right_sub, self.ccw_left_sub], 10, 0.1)
         self.door_proximity_timesync.registerCallback(self.door_proximity_callback)
 
-        self.cw0_pub = rospy.Publisher('cw0', Float64, queue_size=1)
-        self.cw1_pub = rospy.Publisher('cw1', Float64, queue_size=1)
-        self.cw2_pub = rospy.Publisher('cw2', Float64, queue_size=1)
-        self.cw3_pub = rospy.Publisher('cw3', Float64, queue_size=1)
-        self.cw4_pub = rospy.Publisher('cw4', Float64, queue_size=1)
+        self.cw_num_sensors = 5
+        self.cw_pub = [rospy.Publisher('cw%i' % i, Float64, queue_size=1) for i in range(self.cw_num_sensors)]
+        self.ccw_num_sensors = 5
+        self.ccw_pub = [rospy.Publisher('ccw%i' % i, Float64, queue_size=1) for i in range(self.ccw_num_sensors)]
 
-        self.ccw0_pub = rospy.Publisher('ccw0', Float64, queue_size=1)
-        self.ccw1_pub = rospy.Publisher('ccw1', Float64, queue_size=1)
-        self.ccw2_pub = rospy.Publisher('ccw2', Float64, queue_size=1)
-        self.ccw3_pub = rospy.Publisher('ccw3', Float64, queue_size=1)
-        self.ccw4_pub = rospy.Publisher('ccw4', Float64, queue_size=1)
-
-        self.cw0 = 0.0
-        self.cw1 = 0.0
-        self.cw2 = 0.0
-        self.cw3 = 0.0
-        self.cw4 = 0.0
-        self.cw5 = 0.0
-
-        self.ccw0 = 0.0
-        self.ccw1 = 0.0
-        self.ccw2 = 0.0
-        self.ccw3 = 0.0
-        self.ccw4 = 0.0
-        self.ccw5 = 0.0
+        self.cw = np.zeros(self.cw_num_sensors)
+        self.ccw = np.zeros(self.ccw_num_sensors)
 
         self.door_angle = 0.0
 
@@ -66,6 +51,14 @@ class MadrobPreprocess:
         self.ajar_door_angle_th = 0.4
         self.closed_door_angle_th = 0.03
         self.door_height = 2000.0
+
+        # test moving average
+        self.ccw1_clean = deque()
+        self.cw1_clean = deque()
+        self.moving_average_width = 6
+        self.cw1_clean_pub = rospy.Publisher('cw1_clean', Float64, queue_size=1)
+        self.ccw1_clean_pub = rospy.Publisher('ccw1_clean', Float64, queue_size=1)
+        # END test moving average
 
         # cw  → negative angles
         # ccw → positive angles
@@ -101,146 +94,83 @@ class MadrobPreprocess:
         self.door_angle = door.angle
 
     def door_proximity_callback(self, cw_right, cw_left, ccw_right, ccw_left):
-        # door_closed = False
-        # door_open = False
+
         door_ajar_cw = -self.ajar_door_angle_th < self.door_angle < -self.closed_door_angle_th
         door_ajar_ccw = self.closed_door_angle_th < self.door_angle < self.ajar_door_angle_th
-        # last_falling_edge = None
-        # last_falling_edge_time = None
 
-        # if -self.closed_door_angle_th < self.door_angle < self.closed_door_angle_th:
-        #     door_closed = True
-        #
-        # if self.closed_door_angle_th < self.door_angle < self.ajar_door_angle_th:
-        #     door_ajar_ccw = True
-        #
-        # if -self.ajar_door_angle_th < self.door_angle < -self.closed_door_angle_th:
-        #     door_ajar_cw = True
-        #
-        # if self.door_angle > self.ajar_door_angle_th or self.door_angle < -self.ajar_door_angle_th:
-        #     door_open = True
+        new_cw = np.zeros(self.cw_num_sensors)
+        cw_stamps = np.full(fill_value=None, dtype=rospy.Time, shape=(self.cw_num_sensors,))
+        if not door_ajar_cw:
+            new_cw[0] = self.door_height - cw_right.ranges[3].range
+            new_cw[1] = self.door_height - cw_right.ranges[2].range
+            new_cw[2] = self.door_height - cw_right.ranges[1].range
+            new_cw[3] = self.door_height - cw_right.ranges[0].range
+            new_cw[4] = self.door_height - cw_left.ranges[3].range
+            cw_stamps[0:4] = cw_right.header.stamp
+            cw_stamps[4] = cw_left.header.stamp
 
-        if door_ajar_cw:
-            new_cw0 = 0.0
-            new_cw1 = 0.0
-            new_cw2 = 0.0
-            new_cw3 = 0.0
-            new_cw4 = 0.0
-        else:
-            new_cw0 = self.door_height - cw_right.ranges[3].range
-            new_cw1 = self.door_height - cw_right.ranges[2].range
-            new_cw2 = self.door_height - cw_right.ranges[1].range
-            new_cw3 = self.door_height - cw_right.ranges[0].range
-            new_cw4 = self.door_height - cw_left.ranges[3].range
+        new_ccw = np.zeros(self.ccw_num_sensors)
+        ccw_stamps = np.full(fill_value=None, dtype=rospy.Time, shape=(self.ccw_num_sensors,))
+        if not door_ajar_ccw:
+            new_ccw[0] = self.door_height - ccw_left.ranges[3].range
+            new_ccw[1] = self.door_height - ccw_left.ranges[2].range
+            new_ccw[2] = self.door_height - ccw_left.ranges[1].range
+            new_ccw[3] = 0.0  # new_ccw[3] = self.door_height - ccw_left.ranges[0].range  # broken sensor
+            new_ccw[4] = self.door_height - ccw_right.ranges[3].range
+            ccw_stamps[0:4] = ccw_left.header.stamp
+            ccw_stamps[4] = ccw_right.header.stamp
 
-        if door_ajar_ccw:
-            new_ccw0 = 0.0
-            new_ccw1 = 0.0
-            new_ccw2 = 0.0
-            new_ccw3 = 0.0
-            new_ccw4 = 0.0
-        else:
-            new_ccw0 = self.door_height - ccw_left.ranges[3].range
-            new_ccw1 = self.door_height - ccw_left.ranges[2].range
-            new_ccw2 = self.door_height - ccw_left.ranges[1].range
-            # new_ccw3 = self.door_height - ccw_left.ranges[0].range  # broken sensor
-            new_ccw3 = 0.0
-            new_ccw4 = self.door_height - ccw_right.ranges[3].range
+        # test moving average
+        while len(self.cw1_clean) >= self.moving_average_width:
+            self.cw1_clean.popleft()
+        self.cw1_clean.append(new_cw[1])
+        self.cw1_clean_pub.publish(sum(self.cw1_clean)/len(self.cw1_clean))
 
-        if self.cw0 < self.th < new_cw0:
-            print "%.3f\t↑\t\t\t\t\t|" % self.door_angle  # rising edge cw
-        if self.cw0 > self.th > new_cw0:
-            print "%.3f\t↓\t\t\t\t\t|" % self.door_angle  # falling edge cw
-            self.last_falling_edge_side = 'cw'
-            self.last_falling_edge_time = cw_right.header.stamp
+        while len(self.ccw1_clean) >= self.moving_average_width:
+            self.ccw1_clean.popleft()
+        self.ccw1_clean.append(new_ccw[1])
+        self.ccw1_clean_pub.publish(sum(self.ccw1_clean)/len(self.ccw1_clean))
+        # END test moving average
 
-        if self.cw1 < self.th < new_cw1:
-            print "%.3f\t\t↑\t\t\t\t|" % self.door_angle
-        if self.cw1 > self.th > new_cw1:
-            print "%.3f\t\t↓\t\t\t\t|" % self.door_angle
-            self.last_falling_edge_side = 'cw'
-            self.last_falling_edge_time = cw_right.header.stamp
+        for i in range(self.cw_num_sensors):
+            # rising edge on sensor cw i
+            if self.cw[i] < self.th < new_cw[i]:
+                print "{angle:.3f}\t{cw_pre_tabs}↑{cw_post_tabs}\t|".format(angle=self.door_angle,
+                                                                            cw_pre_tabs='\t' * i,
+                                                                            cw_post_tabs='\t' * (self.cw_num_sensors - i - 1))
+            # falling edge on sensor cw i
+            if self.cw[i] > self.th > new_cw[i]:
+                print "{angle:.3f}\t{cw_pre_tabs}↓{cw_post_tabs}\t|".format(angle=self.door_angle,
+                                                                            cw_pre_tabs='\t' * i,
+                                                                            cw_post_tabs='\t' * (self.cw_num_sensors-i-1))
+                self.last_falling_edge_side = 'cw'
+                self.last_falling_edge_time = cw_stamps[i]
 
-        if self.cw2 < self.th < new_cw2:
-            print "%.3f\t\t\t↑\t\t\t|" % self.door_angle
-        if self.cw2 > self.th > new_cw2:
-            print "%.3f\t\t\t↓\t\t\t|" % self.door_angle
-            self.last_falling_edge_side = 'cw'
-            self.last_falling_edge_time = cw_right.header.stamp
+        for i in range(self.ccw_num_sensors):
+            # rising edge on sensor ccw i
+            if self.ccw[i] < self.th < new_ccw[i]:
+                print "{angle:.3f}\t{cw_tabs}|{ccw_tabs}↑".format(angle=self.door_angle,
+                                                                  cw_tabs='\t' * self.cw_num_sensors,
+                                                                  ccw_tabs='\t' * (i + 1))
+            if self.ccw[i] > self.th > new_ccw[i]:
+                print "{angle:.3f}\t{cw_tabs}|{ccw_tabs}↓".format(angle=self.door_angle,
+                                                                  cw_tabs='\t'*self.cw_num_sensors,
+                                                                  ccw_tabs='\t' * (i + 1))
+                self.last_falling_edge_side = 'ccw'
+                self.last_falling_edge_time = ccw_stamps[i]
 
-        if self.cw3 < self.th < new_cw3:
-            print "%.3f\t\t\t\t↑\t\t|" % self.door_angle
-        if self.cw3 > self.th > new_cw3:
-            print "%.3f\t\t\t\t↓\t\t|" % self.door_angle
-            self.last_falling_edge_side = 'cw'
-            self.last_falling_edge_time = cw_right.header.stamp
+        for i in range(self.cw_num_sensors):
+            self.cw[i] = new_cw[i]
 
-        if self.cw4 < self.th < new_cw4:
-            print "%.3f\t\t\t\t\t↑\t|" % self.door_angle
-        if self.cw4 > self.th > new_cw4:
-            print "%.3f\t\t\t\t\t↓\t|" % self.door_angle
-            self.last_falling_edge_side = 'cw'
-            self.last_falling_edge_time = cw_left.header.stamp
+        for i in range(self.ccw_num_sensors):
+            self.ccw[i] = new_ccw[i]
 
+        for i in range(self.cw_num_sensors):
+            self.cw_pub[i].publish(self.cw[i])
 
-        if self.ccw0 < self.th < new_ccw0:
-            print "%.3f\t\t\t\t\t\t|\t↑" % self.door_angle  # rising edge ccw
-        if self.ccw0 > self.th > new_ccw0:
-            print "%.3f\t\t\t\t\t\t|\t↓" % self.door_angle  # falling edge ccw
-            self.last_falling_edge_side = 'ccw'
-            self.last_falling_edge_time = ccw_left.header.stamp
+        for i in range(self.ccw_num_sensors):
+            self.ccw_pub[i].publish(self.ccw[i])
 
-        if self.ccw1 < self.th < new_ccw1:
-            print "%.3f\t\t\t\t\t\t|\t\t↑" % self.door_angle
-        if self.ccw1 > self.th > new_ccw1:
-            print "%.3f\t\t\t\t\t\t|\t\t↓" % self.door_angle
-            self.last_falling_edge_side = 'ccw'
-            self.last_falling_edge_time = ccw_left.header.stamp
-
-        if self.ccw2 < self.th < new_ccw2:
-            print "%.3f\t\t\t\t\t\t|\t\t\t↑" % self.door_angle
-        if self.ccw2 > self.th > new_ccw2:
-            print "%.3f\t\t\t\t\t\t|\t\t\t↓" % self.door_angle
-            self.last_falling_edge_side = 'ccw'
-            self.last_falling_edge_time = ccw_left.header.stamp
-
-        if self.ccw3 < self.th < new_ccw3:
-            print "%.3f\t\t\t\t\t\t|\t\t\t\t↑" % self.door_angle
-        if self.ccw3 > self.th > new_ccw3:
-            print "%.3f\t\t\t\t\t\t|\t\t\t\t↓" % self.door_angle
-            self.last_falling_edge_side = 'ccw'
-            self.last_falling_edge_time = ccw_left.header.stamp
-
-        if self.ccw4 < self.th < new_ccw4:
-            print "%.3f\t\t\t\t\t\t|\t\t\t\t\t↑" % self.door_angle
-        if self.ccw4 > self.th > new_ccw4:
-            print "%.3f\t\t\t\t\t\t|\t\t\t\t\t↓" % self.door_angle
-            self.last_falling_edge_side = 'ccw'
-            self.last_falling_edge_time = ccw_right.header.stamp
-
-        self.cw0 = new_cw0
-        self.cw1 = new_cw1
-        self.cw2 = new_cw2
-        self.cw3 = new_cw3
-        self.cw4 = new_cw4
-
-        self.ccw0 = new_ccw0
-        self.ccw1 = new_ccw1
-        self.ccw2 = new_ccw2
-        self.ccw3 = new_ccw3
-        self.ccw4 = new_ccw4
-
-        self.cw0_pub.publish(self.cw0)
-        self.cw1_pub.publish(self.cw1)
-        self.cw2_pub.publish(self.cw2)
-        self.cw3_pub.publish(self.cw3)
-        self.cw4_pub.publish(self.cw4)
-
-        self.ccw0_pub.publish(self.ccw0)
-        self.ccw1_pub.publish(self.ccw1)
-        self.ccw2_pub.publish(self.ccw2)
-        self.ccw3_pub.publish(self.ccw3)
-        self.ccw4_pub.publish(self.ccw4)
 
     @staticmethod
     def run():
