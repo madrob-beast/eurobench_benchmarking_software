4d41cc03e0d08160bf9c5d62a6a89930f2d04cea
jpereira <joaomacpereira@tecnico.ulisboa.pt>
2020-01-14 17:56:19 +0000

    Separate preprocessing scripts, with access to testbed conf

--- eurobench_benchmark_core/src/benchmark_scripts/preprocess/base_preprocess.py
+++ eurobench_benchmark_core/src/benchmark_scripts/preprocess/base_preprocess.py
@@ -3,8 +3,10 @@
 from exceptions import NotImplementedError
 
 class BasePreprocess(object):
+    def __init__(self, data_type):
+        raise NotImplementedError()
 
-    def start(self, rosbag_path=None):
+    def start(self, benchmark_group, robot_name, run_number, start_time, testbed_conf):
         raise NotImplementedError()
 
     def finish(self):
