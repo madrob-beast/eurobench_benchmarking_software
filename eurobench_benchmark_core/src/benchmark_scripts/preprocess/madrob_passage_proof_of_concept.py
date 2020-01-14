99060684169d7bf223d11fa0922c48cc9f620b9a
Enrico Piazza <enry2077@gmail.com>
2020-01-14 17:04:59 +0100

    added door_opens and humanoid_approaches_door to event list. Also added test for computing velocity and aceleration from door angle (better than NovaLabs values, but acceleration still looks ugly)

--- eurobench_benchmark_core/src/benchmark_scripts/preprocess/madrob_passage_proof_of_concept.py
+++ eurobench_benchmark_core/src/benchmark_scripts/preprocess/madrob_passage_proof_of_concept.py
@@ -14,36 +14,24 @@ class MadrobPreprocess:
     def __init__(self):
         rospy.init_node("passage_test_node")
 
-        self.cw_right_sub = None
-        self.cw_left_sub = None
-        self.ccw_right_sub = None
-        self.ccw_left_sub = None
-
-        self.door_sub = None
-
-        self.door_sub = rospy.Subscriber('/madrob/door/state', Door, self.door_state_callback)
-        self.door_vel_pub = rospy.Publisher('/madrob/door/velocity_test', Float64, queue_size=1)
-
-        self.cw_right_sub = message_filters.Subscriber('/madrob/passage/cw_right', Passage)
-        self.cw_left_sub = message_filters.Subscriber('/madrob/passage/cw_left', Passage)
-        self.ccw_right_sub = message_filters.Subscriber('/madrob/passage/ccw_right', Passage)
-        self.ccw_left_sub = message_filters.Subscriber('/madrob/passage/ccw_left', Passage)
-
-        self.door_proximity_timesync = message_filters.ApproximateTimeSynchronizer([self.cw_right_sub, self.cw_left_sub, self.ccw_right_sub, self.ccw_left_sub], 10, 0.1)
-        self.door_proximity_timesync.registerCallback(self.door_proximity_callback)
-
         self.cw_num_sensors = 5
-        self.cw_pub = [rospy.Publisher('cw%i' % i, Float64, queue_size=1) for i in range(self.cw_num_sensors)]
         self.ccw_num_sensors = 5
-        self.ccw_pub = [rospy.Publisher('ccw%i' % i, Float64, queue_size=1) for i in range(self.ccw_num_sensors)]
-
         self.cw = np.zeros(self.cw_num_sensors)
         self.ccw = np.zeros(self.ccw_num_sensors)
+
+        self.previous_door_angle_msgs = deque()
+        self.previous_angular_velocity = None
+        self.previous_angular_velocity_stamp = None
+        self.previous_angular_acceleration = None
+        self.previous_angular_acceleration_stamp = None
 
         self.door_angle = 0.0
 
         self.last_falling_edge_side = None
         self.last_falling_edge_time = None
+
+        self.first_rising_edge_side = None
+        self.first_rising_edge_time = None
 
         self.events = list()
 
@@ -59,6 +47,22 @@ class MadrobPreprocess:
         self.cw1_clean_pub = rospy.Publisher('cw1_clean', Float64, queue_size=1)
         self.ccw1_clean_pub = rospy.Publisher('ccw1_clean', Float64, queue_size=1)
         # END test moving average
+
+        # Publishers
+        self.angular_velocity_pub = rospy.Publisher('/madrob/door/velocity_test', Float64, queue_size=1)
+        self.angular_acceleration_pub = rospy.Publisher('/madrob/door/acceleration_test', Float64, queue_size=1)
+        self.cw_pub = [rospy.Publisher('cw%i' % i, Float64, queue_size=1) for i in range(self.cw_num_sensors)]
+        self.ccw_pub = [rospy.Publisher('ccw%i' % i, Float64, queue_size=1) for i in range(self.ccw_num_sensors)]
+
+        # Subscribers (last in __init__ to avoid registering the callbacks before the attributes of self are completely instantiated)
+        self.door_sub = rospy.Subscriber('/madrob/door/state', Door, self.door_state_callback)
+        self.cw_right_sub = message_filters.Subscriber('/madrob/passage/cw_right', Passage)
+        self.cw_left_sub = message_filters.Subscriber('/madrob/passage/cw_left', Passage)
+        self.ccw_right_sub = message_filters.Subscriber('/madrob/passage/ccw_right', Passage)
+        self.ccw_left_sub = message_filters.Subscriber('/madrob/passage/ccw_left', Passage)
+        self.door_proximity_timesync = message_filters.ApproximateTimeSynchronizer([self.cw_right_sub, self.cw_left_sub, self.ccw_right_sub, self.ccw_left_sub], 10, 0.1)
+        self.door_proximity_timesync.registerCallback(self.door_proximity_callback)
+
 
         # cw  → negative angles
         # ccw → positive angles
@@ -87,11 +91,36 @@ class MadrobPreprocess:
 
         if was_door_closed and not is_door_closed:
             print "door opens (ajar)"
+            self.events.append((door.header.stamp, 'door_opens'))
         if is_door_closed and not was_door_closed:
             print "door closes"
             self.events.append((door.header.stamp, 'door_closes'))
 
         self.door_angle = door.angle
+
+        # test compute velocity and acceleration
+        current_angular_velocity = None
+        current_angular_velocity_stamp = None
+        if len(self.previous_door_angle_msgs) >= 10:
+            previous_angular_velocity_msg = self.previous_door_angle_msgs.popleft()
+
+            current_angular_velocity = (previous_angular_velocity_msg.angle - door.angle) / (previous_angular_velocity_msg.header.stamp - door.header.stamp).to_sec()
+            current_angular_velocity_stamp = door.header.stamp + (door.header.stamp - previous_angular_velocity_msg.header.stamp) * 0.5
+
+            self.angular_velocity_pub.publish(Float64(data=current_angular_velocity))
+
+        self.previous_door_angle_msgs.append(door)
+
+        # acceleration
+        if current_angular_velocity is not None and self.previous_angular_velocity is not None:
+            current_angular_acceleration = (self.previous_angular_velocity - current_angular_velocity) / (self.previous_angular_velocity_stamp - current_angular_velocity_stamp).to_sec()
+            current_angular_acceleration_stamp = current_angular_velocity_stamp + (current_angular_velocity_stamp - self.previous_angular_velocity_stamp) * 0.5  # (self.previous_angular_velocity.header.stamp + current_angular_velocity_stamp) / 2
+
+            self.angular_acceleration_pub.publish(Float64(data=current_angular_acceleration))
+
+        self.previous_angular_velocity = current_angular_velocity
+        self.previous_angular_velocity_stamp = current_angular_velocity_stamp
+        # END test compute velocity and acceleration
 
     def door_proximity_callback(self, cw_right, cw_left, ccw_right, ccw_left):
 
@@ -138,6 +167,10 @@ class MadrobPreprocess:
                 print "{angle:.3f}\t{cw_pre_tabs}↑{cw_post_tabs}\t|".format(angle=self.door_angle,
                                                                             cw_pre_tabs='\t' * i,
                                                                             cw_post_tabs='\t' * (self.cw_num_sensors - i - 1))
+                if self.first_rising_edge_side is None:
+                    self.first_rising_edge_side = 'cw'
+                    self.first_rising_edge_time = cw_stamps[i]
+
             # falling edge on sensor cw i
             if self.cw[i] > self.th > new_cw[i]:
                 print "{angle:.3f}\t{cw_pre_tabs}↓{cw_post_tabs}\t|".format(angle=self.door_angle,
@@ -152,6 +185,10 @@ class MadrobPreprocess:
                 print "{angle:.3f}\t{cw_tabs}|{ccw_tabs}↑".format(angle=self.door_angle,
                                                                   cw_tabs='\t' * self.cw_num_sensors,
                                                                   ccw_tabs='\t' * (i + 1))
+                if self.first_rising_edge_side is None:
+                    self.first_rising_edge_side = 'ccw'
+                    self.first_rising_edge_time = ccw_stamps[i]
+
             if self.ccw[i] > self.th > new_ccw[i]:
                 print "{angle:.3f}\t{cw_tabs}|{ccw_tabs}↓".format(angle=self.door_angle,
                                                                   cw_tabs='\t'*self.cw_num_sensors,
@@ -182,8 +219,17 @@ class MadrobPreprocess:
             pass
 
     def end(self):
-        self.events.append((self.last_falling_edge_time, 'humanoid_moves_to_{}_side'.format(self.last_falling_edge_side)))
-        print self.events
+        if self.first_rising_edge_side is not None:
+            self.events.append((self.first_rising_edge_time, 'humanoid_approaches_the_door_on_{}_side'.format(self.first_rising_edge_side)))
+
+        if self.last_falling_edge_side is not None:
+            self.events.append((self.last_falling_edge_time, 'humanoid_moves_to_{}_side'.format(self.last_falling_edge_side)))
+
+        # sort by time (first element of the tuple)
+        self.events.sort(key=lambda time_event: time_event[0])
+
+        for time, event in self.events:
+            print time, '\t', event
 
 
 passage_preprocessor = MadrobPreprocess()
