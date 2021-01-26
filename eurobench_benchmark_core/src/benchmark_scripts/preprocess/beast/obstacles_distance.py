2609d6450355e37a55edb4db656ea78949ccd431
Enri2077 <enry2077@gmail.com>
2021-01-26 11:33:26 +0100

    changed data format name range -> distance

--- eurobench_benchmark_core/src/benchmark_scripts/preprocess/beast/obstacles_distance.py
+++ eurobench_benchmark_core/src/benchmark_scripts/preprocess/beast/obstacles_distance.py
@@ -11,9 +11,9 @@ from benchmark_scripts.preprocess.base_preprocess import BasePreprocess
 
 class PreprocessObject(BasePreprocess):
     def __init__(self):
-        super(PreprocessObject, self).__init__(data_format_name="range")
+        super(PreprocessObject, self).__init__(data_format_name="distance")
         self.scan_sub = None
-        self.range_list = None
+        self.distance_list = None
 
     def start(self, benchmark_group, robot_name, condition_number, run_number, start_time, testbed_conf, preprocess_dir):
         self.robot_name = robot_name
@@ -21,17 +21,17 @@ class PreprocessObject(BasePreprocess):
         self.run_number = run_number
         self.preprocess_dir = preprocess_dir
 
-        self.range_list = list()
+        self.distance_list = list()
         self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
 
     def finish(self):
         self.scan_sub.unregister()
 
         preprocess_file_path = self.preprocessed_csv_file_path()
-        df = pd.DataFrame(columns=['time', 'range'], data=self.range_list)
-        df[['time', 'range']].to_csv(preprocess_file_path, index=False)
+        df = pd.DataFrame(columns=['time', 'distance'], data=self.distance_list)
+        df[['time', 'distance']].to_csv(preprocess_file_path, index=False)
 
         return self.data_format_name, preprocess_file_path
 
     def scan_callback(self, scan_msg):
-        self.range_list.append((scan_msg.header.stamp.to_sec(), np.min(scan_msg.ranges)))
+        self.distance_list.append((scan_msg.header.stamp.to_sec(), np.min(scan_msg.ranges)))
