d35ee413d49a12de78512628edb6048942b06ca2
Enri2077 <enry2077@gmail.com>
2021-04-06 10:00:23 +0200

    wip: install scripts

---
+++ eurobench_benchmark_core/scripts/madrob_install_BMS_dependences_GUI_PC.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/catkin_ws/src/
+
+git clone https://github.com/madrob-beast/eurobench_benchmarking_software.git
+git clone https://github.com/madrob-beast/madrob_beast_pi.git
+git clone https://github.com/madrob-beast/madrob_msgs.git
+git clone https://github.com/madrob-beast/madrob_srvs.git
+git clone https://github.com/madrob-beast/beast_msgs.git
+git clone https://github.com/madrob-beast/beast_srvs.git
+
---
+++ eurobench_benchmark_core/scripts/madrob_install_BMS_dependences_onboard_PC.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/catkin_ws/src/
+
+git clone https://github.com/madrob-beast/eurobench_benchmarking_software.git
+git clone https://github.com/madrob-beast/beast_srvs.git
+git clone https://github.com/madrob-beast/beast_msgs.git
+git clone https://github.com/madrob-beast/madrob_beast_pi.git
+
