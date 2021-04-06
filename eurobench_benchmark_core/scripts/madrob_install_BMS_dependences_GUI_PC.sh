9a5f2dcb3d2f62bbb231768457654320906b63f5
Enrico Piazza <809244+Enri2077@users.noreply.github.com>
2021-04-06 11:06:35 +0200

    Install scripts (#24)
    * wip: install scripts
    
    * corrected install scripts and package deps

---
+++ eurobench_benchmark_core/scripts/madrob_install_BMS_dependences_GUI_PC.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ~/catkin_ws/src/
+
+git clone https://github.com/madrob-beast/eurobench_benchmarking_software.git
+git clone https://github.com/madrob-beast/madrob_beast_pi.git
+git clone https://github.com/madrob-beast/madrob.git
+git clone https://github.com/madrob-beast/madrob_msgs.git
+git clone https://github.com/madrob-beast/madrob_srvs.git
+git clone https://github.com/madrob-beast/beast.git
+git clone https://github.com/madrob-beast/beast_msgs.git
+git clone https://github.com/madrob-beast/beast_srvs.git
+
+sudo apt install -y python-pandas
+
+# install latest version of PyYAML and prevent the error:
+#    AttributeError: 'module' object has no attribute 'FullLoader'
+sudo apt install -y python-pip
+pip install -U PyYAML
+
