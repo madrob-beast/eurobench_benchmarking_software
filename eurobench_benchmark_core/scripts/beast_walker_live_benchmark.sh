02dfba74ebd0f626b8ae2fae0dce7857f9960bd8
Enri2077 <enry2077@gmail.com>
2021-09-21 16:45:52 +0200

    added distinction between cart and walker for gui

--- eurobench_benchmark_core/scripts/beast_walker_live_benchmark.sh
+++ eurobench_benchmark_core/scripts/beast_walker_live_benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export ROS_IP='10.0.0.176'  # The IP of the GUI PC
+export ROS_IP='10.0.0.177'  # The IP of the GUI PC
 export ROS_MASTER_URI='http://10.0.0.128:11311'  # The IP of the BEAST onboard PC
-roslaunch eurobench_benchmark_core beast_gui.launch
+roslaunch eurobench_benchmark_core beast_walker_gui.launch
