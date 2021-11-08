9f31bb84a9bed4e84e9a710d8369da73a881a6e6
Enri2077 <enry2077@gmail.com>
2021-11-08 14:10:29 +0100

    updated GUI PC and beast IPs

--- eurobench_benchmark_core/scripts/beast_walker_live_benchmark.sh
+++ eurobench_benchmark_core/scripts/beast_walker_live_benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export ROS_IP='10.0.0.177'  # The IP of the GUI PC
-export ROS_MASTER_URI='http://10.0.0.128:11311'  # The IP of the BEAST onboard PC
+export ROS_IP='192.168.1.100'  # The IP of the GUI PC
+export ROS_MASTER_URI='http://192.168.1.2:11311'  # The IP of the BEAST onboard PC
 roslaunch eurobench_benchmark_core beast_walker_gui.launch
