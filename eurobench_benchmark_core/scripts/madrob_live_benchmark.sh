5c56a7f101f4f4ffb5a57f985ffd8f8893d54927
Enri2077 <enry2077@gmail.com>
2022-05-09 16:14:37 +0200

    set correct IP and master uri for madrob GUI PC

--- eurobench_benchmark_core/scripts/madrob_live_benchmark.sh
+++ eurobench_benchmark_core/scripts/madrob_live_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#export ROS_IP='TODO'  # The IP of the GUI PC
-export ROS_HOSTNAME='TODO'  # The hostname of the GUI PC
-export ROS_MASTER_URI='http://madrob:11311'  # The ROS master URI of the BEAST onboard PC
+export ROS_IP='192.168.0.10'  # The IP of the GUI PC
+#export ROS_HOSTNAME='beast-madrob-gui'  # The hostname of the GUI PC
+export ROS_MASTER_URI='http://192.168.0.2:11311'  # The ROS master URI of the MADROB onboard PC
 roslaunch eurobench_benchmark_core madrob_gui.launch
